fdaa25f4674412d63dcff49bbff4d56178a90dac
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-08-15 13:46:58 -0400

    Add method stubs

---
+++ lib/screens/ideal_gas_law.dart
@@ -0,0 +1,21 @@
+import 'package:decimal/decimal.dart';
+
+/// P = n(RT/V)
+Decimal _computePressure() {
+  return Decimal.zero;
+}
+
+/// V = n(RT)/P
+Decimal _computeVolume() {
+  return Decimal.zero;
+}
+
+/// n = (PV)/(RT)
+Decimal _computeAmountOfSubstance() {
+  return Decimal.zero;
+}
+
+/// T = (PV)/(nR)
+Decimal _computeTemperature() {
+  return Decimal.zero;
+}
