e634101058e4651a09201768a036efd9216d7e3c
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-08-15 17:35:27 -0400

    Add grams moles calculator to screens file (#25)
    * Add solutions_molarity.dart
    
    * Add solutions_molarity.dart to the routes, screens, and drawer
    
    * Add grams & moles calculator to the screens file

--- lib/screens.dart
+++ lib/screens.dart
@@ -1,3 +1,5 @@
 export 'screens/home_screen.dart';
 export 'screens/molar_mass.dart';
+export 'screens/grams_moles.dart';
+export 'screens/solutions_molarity.dart';
 export 'screens/text_input_screen.dart';
