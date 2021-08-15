3837eee1e35e9a548031807f8133daac79f4323e
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-08-15 18:49:29 -0400

    Added some more calculation helpers

--- lib/util/calculation_helpers.dart
+++ lib/util/calculation_helpers.dart
@@ -2,6 +2,7 @@ import 'package:decimal/decimal.dart';
 import 'package:more/more.dart';
 import 'package:periodic_table/periodic_table.dart';
 
+//<editor-fold desc="Grams, moles, and all the good stuff">
 /// Calculates the molar mass of a single element or a list of elements.
 /// [elementsAndQuantities] is a list of pairs where the left side is
 /// a [ChemicalElement] and the right side is the number of atoms of that
@@ -17,3 +18,30 @@ Decimal calculateMolarMass(
   }
   return out;
 }
+//</editor-fold>
+
+//<editor-fold desc="Solutions and molarity">
+/// For a solution, given the [concentration] (in molarity), [volume] of the
+/// solution in liters, and [molarMassOfSolute], returns the mass of solute
+/// needed to make the solution.
+Decimal massOfSolute(
+    Decimal concentration, Decimal volume, Decimal molarMassOfSolute) {
+  return concentration * volume * molarMassOfSolute;
+}
+
+/// For a solution given the [massOfSolute] in grams, the [molarMassOfSolute],
+/// and [volume] of the solution in liters, returns the concentration of the
+/// solution (in molarity).
+Decimal concentrationOfSolution(
+    Decimal massOfSolute, Decimal volume, Decimal molarMassOfSolute) {
+  return (massOfSolute / molarMassOfSolute) * volume;
+}
+
+/// For a solution given the [massOfSolute] in grams, the [concentration]
+/// (in molarity), and the [molarMassOfSolute], returns the volume of the
+/// solution in liters.
+Decimal volumeOfSolution(
+    Decimal massOfSolute, Decimal concentration, Decimal molarMassOfSolute) {
+  return massOfSolute / concentration / molarMassOfSolute;
+}
+//</editor-fold>
