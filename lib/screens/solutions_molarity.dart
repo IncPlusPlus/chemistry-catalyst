450fa1c5f804579c8433618cb049afda0fd0c47c
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-08-15 21:23:45 -0400

    Holy shit it's working
    IT'S WORKING!!!

--- lib/screens/solutions_molarity.dart
+++ lib/screens/solutions_molarity.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:chem_catalyst/model/element_item.dart';
+import 'package:chem_catalyst/util/calculation_helpers.dart';
 import 'package:chem_catalyst/util/string_helpers.dart';
 import 'package:chem_catalyst/widgets.dart';
 import 'package:decimal/decimal.dart';
@@ -23,6 +24,34 @@ class _SolutionsAndMolarityState extends State<SolutionsAndMolarity> {
   Decimal volume = Decimal.zero;
   Decimal mass = Decimal.zero;
   UnknownVariable solvingFor = UnknownVariable.mass;
+  late Decimal molarMass = calculateMolarMass(elements);
+
+  final molarMassController = TextEditingController();
+  final massController = TextEditingController();
+  final concentrationController = TextEditingController();
+  final volumeController = TextEditingController();
+
+  bool invalidMassInput = false;
+  bool invalidConcentrationInput = false;
+  bool invalidVolumeInput = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start listening to changes.
+    massController.addListener(_setMass);
+    concentrationController.addListener(_setConcentration);
+    volumeController.addListener(_setVolume);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    massController.dispose();
+    concentrationController.dispose();
+    volumeController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -65,6 +94,7 @@ class _SolutionsAndMolarityState extends State<SolutionsAndMolarity> {
                           onChanged: (UnknownVariable? newValue) =>
                               setState(() {
                                 solvingFor = newValue!;
+                                _clearControllerValue(newValue);
                               }),
                           items: UnknownVariable.values
                               .map<DropdownMenuItem<UnknownVariable>>(
@@ -78,12 +108,95 @@ class _SolutionsAndMolarityState extends State<SolutionsAndMolarity> {
             ),
             Center(
               child: AutoSizeText(
-                "Chemical in question is: ${compoundFormulaString(elements)}",
+                "The ${solvingFor.name} of ${molarMass == Decimal.zero ? '...' : compoundFormulaString(elements)} is ${_getAnswerString()}",
                 style: TextStyle(
                   color: Theme.of(context).unselectedWidgetColor,
-                  fontSize: 24,
+                  fontSize: 20,
                 ),
                 textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      enabled: false,
+                      readOnly: true,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9\.]'))
+                      ],
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                          labelText:
+                              'Molar mass of ${molarMass == Decimal.zero ? '...' : compoundFormulaString(elements)}'),
+                      controller: molarMassController,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9\.]'))
+                      ],
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        labelText:
+                            'Mass of ${molarMass == Decimal.zero ? '...' : compoundFormulaString(elements)}',
+                        errorText: invalidMassInput
+                            ? 'Invalid input. Try a normal number.'
+                            : null,
+                      ),
+                      controller: massController,
+                      enabled: _textFieldEnabled(UnknownVariable.mass),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9\.]'))
+                      ],
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        labelText:
+                            'Concentration of ${molarMass == Decimal.zero ? '...' : compoundFormulaString(elements)}',
+                        errorText: invalidConcentrationInput
+                            ? 'Invalid input. Try a normal number.'
+                            : null,
+                      ),
+                      controller: concentrationController,
+                      enabled: _textFieldEnabled(UnknownVariable.concentration),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9\.]'))
+                      ],
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        labelText:
+                            'Volume of ${molarMass == Decimal.zero ? '...' : compoundFormulaString(elements)}',
+                        errorText: invalidVolumeInput
+                            ? 'Invalid input. Try a normal number.'
+                            : null,
+                      ),
+                      controller: volumeController,
+                      enabled: _textFieldEnabled(UnknownVariable.volume),
+                    ),
+                  ),
+                ],
               ),
             ),
             Flexible(
@@ -132,28 +245,116 @@ class _SolutionsAndMolarityState extends State<SolutionsAndMolarity> {
         ),
       );
 
+  String _getAnswerString() {
+    String out = _getAnswer().toString();
+    switch (solvingFor) {
+      case UnknownVariable.mass:
+        out += ' grams';
+        break;
+      case UnknownVariable.concentration:
+        out += ' (molarity)';
+        break;
+      case UnknownVariable.volume:
+        out += ' liters';
+        break;
+    }
+    return out;
+  }
+
+  Decimal _getAnswer() {
+    switch (solvingFor) {
+      case UnknownVariable.mass:
+        return massOfSolute(concentration, volume, molarMass);
+      case UnknownVariable.concentration:
+        return concentrationOfSolution(mass, volume, molarMass);
+      case UnknownVariable.volume:
+        return volumeOfSolution(mass, concentration, molarMass);
+    }
+  }
+
   void _addElement(ChemicalElement element) {
     setState(() {
-      // Decimal.parse(element.atomicMass.toString())
       elements.add(new Tuple2(element, 0));
+      _setMolarMass();
     });
   }
 
   void _removeListItem(int index) {
     setState(() {
       elements = List.from(elements)..removeAt(index);
+      _setMolarMass();
     });
   }
 
   void _setNumberOfElement(int index, int count) {
     setState(() {
       elements[index] = elements[index].withSecond(count);
+      _setMolarMass();
     });
   }
 
   bool _textFieldEnabled(UnknownVariable myField) {
     UnknownVariable currentlyUnknownVariable = solvingFor;
-    return currentlyUnknownVariable != solvingFor;
+    return currentlyUnknownVariable != myField;
+  }
+
+  void _setMolarMass() {
+    molarMass = calculateMolarMass(elements);
+    molarMassController.text =
+        molarMass == Decimal.zero ? '' : molarMass.toString();
+  }
+
+  void _setMass() {
+    setState(() {
+      try {
+        mass = massController.text == ""
+            ? Decimal.zero
+            : Decimal.parse(massController.text);
+        invalidMassInput = false;
+      } catch (e) {
+        invalidMassInput = true;
+      }
+    });
+  }
+
+  void _setConcentration() {
+    setState(() {
+      try {
+        concentration = concentrationController.text == ""
+            ? Decimal.zero
+            : Decimal.parse(concentrationController.text);
+        invalidConcentrationInput = false;
+      } catch (e) {
+        invalidConcentrationInput = true;
+      }
+    });
+  }
+
+  void _setVolume() {
+    setState(() {
+      try {
+        volume = volumeController.text == ""
+            ? Decimal.zero
+            : Decimal.parse(volumeController.text);
+        invalidVolumeInput = false;
+      } catch (e) {
+        invalidVolumeInput = true;
+      }
+    });
+  }
+
+  void _clearControllerValue(UnknownVariable newValue) {
+    switch (newValue) {
+      case UnknownVariable.mass:
+        massController.text = '';
+        break;
+      case UnknownVariable.concentration:
+        concentrationController.text = '';
+        break;
+      case UnknownVariable.volume:
+        volumeController.text = '';
+        break;
+    }
   }
 }
 
